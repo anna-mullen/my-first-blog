33809604c5b441eb9955161d845bb9ca9840b084
Anna <anna@wovendata.com>
2023-12-21 14:07:29 -0600

    Added css file and template extending

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'), Why this was throwing the error "no patterns or circular import", IDK, seems like the same line to me...
     path('', views.post_list, name='post_list'),
 ]
