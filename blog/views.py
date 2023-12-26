765051629ba56f532ee44744ccb4d53c785ac017
Anna <anna@wovendata.com>
2023-12-26 12:12:59 -0600

    Changed the ordering of queryset for post_list in views.py

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import redirect
 
 # view of a list of posts
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 # view of a single post
